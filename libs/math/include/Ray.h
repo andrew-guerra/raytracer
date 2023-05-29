3911907f27212eb6d429e85c2907a2cd27bb9184
Andrew Guerra <andrewjguerra10@gmail.com>
2023-05-29 15:36:02 -0500

    Implement ray math

--- libs/math/include/Ray.h
+++ libs/math/include/Ray.h
@@ -1,6 +1,8 @@
 #ifndef RAY_H
 #define RAY_H
 
+#include "Vector3.h"
+
 /**
  * @class Ray
  * @brief Represents an 3D ray
@@ -10,6 +12,72 @@
  * all standard ray operations.
 */
 class Ray {
+    public:
+        /**
+         * @brief Constructor assigns origin and direction to the Ray object
+         * @param origin The origin vector of the Ray object
+         * @param direction The direction vector of the Ray object
+        */
+        Ray(Vector3* origin, Vector3* direction);
+
+        /**
+         * @brief Constructor for default Ray object
+        */
+        Ray();
+
+        /**
+         * @brief Copy constructor for Ray object
+         * @param copyRay The ray to be copied
+        */
+        Ray(const Ray& copyRay);
+
+        /**
+         * @brief Destructor for Ray object
+        */
+        ~Ray();
+
+        /**
+         * @brief Returns origin vector of the Ray object
+         * @return The origin vector of the Ray object
+        */
+        Vector3* getOrigin();
+
+        /**
+         * @brief Returns direction vector of the Ray object
+         * @return The direction vector of the Ray object
+        */
+        Vector3* getDirection();
+
+        /**
+         * @brief Returns point along the Ray object's direction and origin based on scaler
+         * @param scaler The scaler for the direction vector
+         * @return The point along the Ray object's direction and origin based on scaler
+        */
+        Vector3 getMemberPoint(float scaler);
+
+        /**
+         * @brief Scales the Ray object's direction vector by scaler
+         * @param scaler The scaler for the direction vector 
+        */
+        void scale(float scaler);
+
+        /**
+         * @brief Rotates the Ray object's direction vector by xTheta, yTheta, and zTheta
+         * @param xTheta The rotation for the direction vector around the X axis
+         * @param yTheta The rotation for the direction vector around the Y axis 
+         * @param zTheta The rotation for the direction vector around the Z axis 
+        */
+        void rotate(float xTheta, float yTheta, float zTheta);
+
+        /**
+         * @brief Translates the Ray object's origin vector by translationVec
+         * @param translationVec The translation vector for the translation
+        */
+        void translate(Vector3 translationVec);
+
+    private:
+        Vector3* origin;
+        Vector3* direction;
 };
 
 #endif
--- libs/math/src/Ray.cc
+++ libs/math/src/Ray.cc
@@ -1,1 +1,51 @@
 #include "../include/Ray.h"
+
+Ray::Ray(Vector3* origin, Vector3* direction) {
+    this->origin = origin;
+    this->direction = direction;
+}
+
+Ray::Ray() {
+    this->origin = new Vector3();
+    this->direction = new Vector3();
+}
+
+Ray::Ray(const Ray& copyRay) {
+    this->origin = copyRay.origin;
+    this->direction = copyRay.direction;
+}
+
+Ray::~Ray() {
+    delete this->origin;
+    delete this->direction;
+}
+
+Vector3* Ray::getOrigin() {
+    return this->origin;
+}
+
+Vector3* Ray::getDirection() {
+    return this->direction;
+}
+
+Vector3 getDirection();
+
+Vector3 Ray::getMemberPoint(float scaler) {
+    return *this->origin + (*this->direction * scaler);
+}
+
+void Ray::scale(float scaler) {
+    this->direction->multiplyCoefficient(scaler);
+}
+
+void Ray::rotate(float xTheta, float yTheta, float zTheta) {
+    Vector3 directionRotated(this->direction->getX(), this->direction->getY() * std::cos(xTheta) - this->direction->getZ() * std::sin(xTheta), this->direction->getY() * std::sin(xTheta) + this->direction->getZ() * std::cos(xTheta));
+    directionRotated = Vector3(this->direction->getX() * std::cos(yTheta) + this->direction->getZ() * std::sin(yTheta), this->direction->getY(), -this->direction->getX() * std::sin(yTheta) + this->direction->getZ() * std::cos(xTheta));
+    directionRotated = Vector3(this->direction->getX() * std::cos(zTheta) - this->direction->getY() * std::sin(zTheta), this->direction->getX() * std::sin(zTheta) + this->direction->getY() * std::cos(zTheta), this->direction->getZ());
+
+    this->direction = &directionRotated;
+}
+
+void Ray::translate(Vector3 translationVec){
+    this->origin->add(translationVec);
+}
