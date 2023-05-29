b760d191865032205810c72b212fdb50cfd9b151
Andrew Guerra <andrewjguerra10@gmail.com>
2023-05-29 15:34:00 -0500

    Implement vector math

--- libs/math/include/Vector3.h
+++ libs/math/include/Vector3.h
@@ -1,6 +1,9 @@
 #ifndef VECTOR3_H
 #define VECTOR3_H
 
+#include <cmath>
+#include <algorithm>
+
 /**
  * @class Vector3
  * @brief Represents an 3D vector
@@ -9,6 +12,169 @@
  * all standard vector operations. 
 */
 class Vector3 {
+    public:
+        /**
+         * @brief Constructor assigns entries to the Vector3 object
+         * @param x The x entry of the Vector3 object
+         * @param y The y entry of the Vector3 object
+         * @param z The z entry of the Vector3 object
+        */
+        Vector3(float x, float y, float z);
+
+        /**
+         * @brief Constructor for default Vector3 object
+        */
+        Vector3();
+
+        /**
+         * @brief Copy constructor for Vector3 object
+         * @param copyVec The vector to be copied
+        */
+        Vector3(const Vector3& copyVec);
+        
+        /**
+         * @brief Destructor for Vector3 object
+        */
+        ~Vector3();
+
+        /**
+         * @brief Returns the x entity of the Vector3 object
+         * @return The x entity of the Vector3 object
+        */
+        float getX();
+
+        /**
+         * @brief Returns the y entity of the Vector3 object
+         * @return The y entity of the Vector3 object
+        */
+        float getY();
+
+        /**
+         * @brief Returns the z entity of the Vector3 object
+         * @return The z entity of the Vector3 object
+        */
+        float getZ();
+
+        /**
+         * @brief Returns magnitude of Vector3 object
+        */
+        float magnitude();
+
+        /**
+         * @brief Returns distance between the Vector3 object and the Vector3 otherVec
+         * @param otherVec The vector to be calculated distance between
+         * @return The distance between the Vector3 object and the Vector3 otherVec
+        */
+        float distance(Vector3 otherVec);
+
+        /**
+         * @brief Noramlizes the Vector3 object
+        */
+        void noramlize();
+
+        /**
+         * @brief Negates the entries in the Vector3 object
+        */
+        void negate();
+
+        /**
+         * @brief Negates the entries in the Vector3 object
+         * @param xMax The maximum value of x to clamp
+         * @param yMax The maximum value of y to clamp
+         * @param zMax The maximum value of z to clamp
+        */
+        void clamp(float xMax, float yMax, float zMax);
+
+        /**
+         * @brief Adds the Vector3 otherVec to the Vector3 object
+         * @param otherVec The vector to be added to the Vector3 object
+        */
+        void add(Vector3 otherVec);
+
+        /**
+         * @brief Subtracts the Vector3 otherVec to the Vector3 object
+         * @param otherVec The vector to be subtracted to the Vector3 object
+        */
+        void subtract(Vector3 otherVec);
+
+        /**
+         * @brief Raises the entries in the Vector3 object to exponent
+         * @param exponent The exponent to raise the Vector3 object to
+        */
+        void power(float exponent);
+
+        /**
+         * @brief Raises the entries in the Vector3 object to the second power
+        */
+        void square();
+
+        /**
+         * @brief Multiplies the entries in the Vector3 object by coefficient
+         * @param coefficient The coefficient to multiply the Vector3 object by
+        */
+        void multiplyCoefficient(float coefficient);
+
+        /**
+         * @brief Returns the Vector3 object made by the dot product of the Vector3 object and the Vector3 otherVec
+         * @param otherVec The vector to multiply by dot product
+         * @return The Vector3 object made by the dot product of the Vector3 object and the Vector3 otherVec
+        */
+        float dotProduct(Vector3 otherVec);
+
+        /**
+         * @brief Returns the Vector3 object made by the cross product of the Vector3 object and the Vector3 otherVec
+         * @param otherVec The vector to multiply by cross product
+         * @return The Vector3 object made by the cross product of the Vector3 object and the Vector3 otherVec
+        */
+        Vector3 crossProduct(Vector3 otherVec);
+
+        /**
+         * @brief Returns the Vector3 object made by the average of the Vector3 object and the Vector3 otherVec
+         * @param otherVec The vector to average by
+         * @return The Vector3 object made by the average of the Vector3 object and the Vector3 otherVec
+        */
+        Vector3 average(Vector3 otherVec);
+
+        /**
+         * @brief Returns the Vector3 object made by the sum of the Vector3 object and the Vector3 otherVec
+         * @param otherVec The vector to add by
+         * @return The Vector3 object made by the sum of the Vector3 object and the Vector3 otherVec
+        */
+        Vector3 operator+(const Vector3& otherVec);
+
+        /**
+         * @brief Returns the Vector3 object made by the difference of the Vector3 object and the Vector3 otherVec
+         * @param otherVec The vector to subtract by
+         * @return The Vector3 object made by the difference of the Vector3 object and the Vector3 otherVec
+        */
+        Vector3 operator-(const Vector3& otherVec);
+
+        /**
+         * @brief Returns the Vector3 object made by the product of the Vector3 object and coefficient
+         * @param coefficient The coefficient to multiply by
+         * @return The Vector3 object made by the product of the Vector3 object and coefficient
+        */
+        Vector3 operator*(float coefficient);
+
+        /**
+         * @brief Returns if the Vector3 object and the Vector3 object are equal within delta
+         * @param otherVec The vector to compare
+         * @param delta The delta that entries can be contained within for equality 
+         * @return The boolean value for if the Vector3 object and the Vector3 object are equal within delta
+        */
+        bool equals(Vector3 otherVec, float delta);
+
+        /**
+         * @brief Returns if the Vector3 object and the Vector3 object are equal within a delta
+         * @param otherVec The vector to compare
+         * @return The boolean value for if the Vector3 object and the Vector3 object are equal within a delta
+        */
+        bool equals(Vector3 otherVec);
+
+    private:
+        float x;
+        float y;
+        float z;
 };
 
 #endif
--- libs/math/src/Vector3.cc
+++ libs/math/src/Vector3.cc
@@ -1,1 +1,146 @@
 #include "../include/Vector3.h"
+
+Vector3::Vector3(float x, float y, float z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+Vector3::Vector3() {
+    this->x = 0;
+    this->y = 0;
+    this->z = 0;
+}
+
+Vector3::Vector3(const Vector3& copyVec) {
+    this->x = copyVec.x;
+    this->y = copyVec.y;
+    this->z = copyVec.z;
+}
+
+Vector3::~Vector3() {
+
+}
+
+float Vector3::getX() {
+    return this->x;
+}
+
+float Vector3::getY() {
+    return this->y;
+}
+
+float Vector3::getZ() {
+    return this->z;
+}
+
+float Vector3::magnitude() {
+    return std::sqrt(this->dotProduct(*this));
+}
+
+float Vector3::distance(Vector3 otherVec) {
+    return (*this - otherVec).magnitude();
+}
+
+void Vector3::noramlize() {
+    float magnitude = this->magnitude();
+
+    this->x /= magnitude;
+    this->y /= magnitude;
+    this->z /= magnitude;
+}
+
+void Vector3::negate() {
+    this->multiplyCoefficient(-1.0);
+}
+
+void Vector3::clamp(float xMax, float yMax, float zMax) {
+    this->x = std::min(this->x, xMax);
+    this->y = std::min(this->y, yMax);
+    this->z = std::min(this->z, zMax);
+}
+
+void Vector3::add(Vector3 otherVec) {
+    this->x += otherVec.x;
+    this->y += otherVec.y;
+    this->z += otherVec.z;
+}
+
+void Vector3::subtract(Vector3 otherVec) {
+    this->x -= otherVec.x;
+    this->y -= otherVec.y;
+    this->z -= otherVec.z;
+}
+
+void Vector3::power(float exponent) {
+    this->x = std::pow(this->x, exponent);
+    this->y = std::pow(this->y, exponent);
+    this->z = std::pow(this->z, exponent);
+}
+
+void Vector3::square() {
+    this->x *= this->x;
+    this->y *= this->y;
+    this->z *= this->z;
+}
+
+void Vector3::multiplyCoefficient(float coefficient) {
+    this->x *= coefficient;
+    this->y *= coefficient;
+    this->z *= coefficient;
+}
+
+float Vector3::dotProduct(Vector3 otherVec) {
+    return this->x * this->x + this->y * this->y + this->z * this->z;
+}
+
+Vector3 Vector3::crossProduct(Vector3 otherVec) {
+    float xNew = this->y * otherVec.z - this->z * otherVec.y;
+    float yNew = this->z * otherVec.x - this->x * otherVec.z;
+    float zNew = this->x * otherVec.y - this->y * otherVec.x;
+
+    return Vector3(xNew, yNew, zNew);
+}
+
+Vector3 Vector3::average(Vector3 otherVec) {
+    float xNew = (this->x + otherVec.x) / 2.0;
+    float yNew = (this->y + otherVec.y) / 2.0;
+    float zNew = (this->z + otherVec.z) / 2.0;
+
+    return Vector3(xNew, yNew, zNew);
+}
+
+Vector3 Vector3::operator+(const Vector3& otherVec) {
+    float xNew = this->x + otherVec.x;
+    float yNew = this->y + otherVec.y;
+    float zNew = this->z + otherVec.z;
+
+    return Vector3(xNew, yNew, zNew);
+}
+
+Vector3 Vector3::operator-(const Vector3& otherVec) {
+    float xNew = this->x - otherVec.x;
+    float yNew = this->y - otherVec.y;
+    float zNew = this->z - otherVec.z;
+
+    return Vector3(xNew, yNew, zNew);
+}
+
+Vector3 Vector3::operator*(float coefficient) {
+    float xNew = this->x * coefficient;
+    float yNew = this->y * coefficient;
+    float zNew = this->z * coefficient;
+
+    return Vector3(xNew, yNew, zNew);
+}
+
+bool Vector3::equals(Vector3 otherVec, float delta) {
+    return std::abs(this->x - otherVec.x) <= delta &&
+           std::abs(this->y - otherVec.y) <= delta &&
+           std::abs(this->z - otherVec.z) <= delta;
+                                                 
+}
+
+bool Vector3::equals(Vector3 otherVec) {
+    return this->equals(otherVec, 0.01);
+}
